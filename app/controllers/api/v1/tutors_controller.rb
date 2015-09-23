9c85b6a81cf64566096e4b33c2ccc168eabb6a27
María Emilia Corrons <ecorrons@cespi.unlp.edu.ar>
2015-09-23 01:01:22 -0300

    database.yml example

--- app/controllers/api/v1/tutors_controller.rb
+++ app/controllers/api/v1/tutors_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::TutorsController < Api::V1::ApiController
   before_action :set_person, only: [:show]
 
   def show
-   # consulta es postman: http://localhost:3000/api/v1/tutor/3
+   # consulta es postman: http://localhost:3000/api/v1/tutors/3
     render json: {error: "Invalid tutor id.", status: 404}, status: :not_found if @tutor.nil?
   end
 
